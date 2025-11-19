ea0ebeec4d7b1e5957ebca3f2b8c51358f993bdf
Davide Canale <davide.canale@taglio.it>
2025-11-19 14:35:38 +0100

    Modificato script per la generazione delle tabelle

--- php/databasecreation.sql
+++ php/databasecreation.sql
@@ -8,7 +8,7 @@ COLLATE utf8mb4_general_ci;
 USE my_espooclicker;
 
 -- 3. CREAZIONE TABELLA UTENTI (Login e Salvataggi)
-CREATE TABLE users (
+CREATE TABLE users_production (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
@@ -17,7 +17,23 @@ CREATE TABLE users (
 );
 
 -- 4. CREAZIONE TABELLA CLASSIFICA (Solo Punteggi Pubblici)
-CREATE TABLE leaderboard (
+CREATE TABLE leaderboard_production (
+    username VARCHAR(50) NOT NULL PRIMARY KEY,
+    score BIGINT DEFAULT 0,
+    prestigeLevel INT DEFAULT 0,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE users_dev (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    save_data LONGTEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- 4. CREAZIONE TABELLA CLASSIFICA (Solo Punteggi Pubblici)
+CREATE TABLE leaderboard_dev (
     username VARCHAR(50) NOT NULL PRIMARY KEY,
     score BIGINT DEFAULT 0,
     prestigeLevel INT DEFAULT 0,
