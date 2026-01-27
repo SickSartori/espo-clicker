f02e12ca8ea160bf32527814ddfbc937befadb2e
Stefano Esposito <stefano.esposito@taglio.it>
2026-01-27 11:42:29 +0100

    v1.0 - Stable: rimosso debug_parameter.php da prod

--- Espo_Clicker/php/debug_parameter.php
+++
@@ -1,6 +0,0 @@
-<?php
-	error_reporting(0); // Setta il livello di errore php da visualizzare a schermo (0 -> nessuno, E_ALL -> tutti)
-	ini_set('ignore_repeated_errors', TRUE); // always use TRUE
-	ini_set('display_errors', FALSE); // Error/Exception display, use FALSE only in production environment or real server. Use TRUE in development environment
-	ini_set('log_errors', TRUE); // Error/Exception file logging engine
-?>
