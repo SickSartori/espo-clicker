40df9813f8ff8f3f6d07b7d2be0f51280b5a26a6
SickSartori <sartori.gabriel1999@gmail.com>
2026-02-15 14:18:16 +0100

    v2.0: Apertura Versione - MAJOR

--- php/config.php
+++ php/config.php
@@ -6,7 +6,7 @@ return [
     "dbname" => "my_espooclicker",
     "port" => 3306,
     "instanceName" => "dev", // 'dev' o 'production'
-    "devVersion" => "1.3.0",
+    "devVersion" => "2.0.0",
     "prodVersion" => "1.1.0"
 ];
 ?>
