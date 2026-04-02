b71827976f26e100327bfe611afefbab7af33ec3
GabrielSartori <gabriel.sartori@taglio.it>
2026-04-02 15:54:32 +0200

    v2.0: Fix Songs load - Server

--- music/get_songs.php
+++ music/get_songs.php
@@ -1,5 +1,5 @@
 <?php
-header('Content-Type: application/json');
+header('Content-Type: application/json; charset=utf-8');
 
 $dir = 'songs/';
 $songs = [];
@@ -10,14 +10,20 @@ if (is_dir($dir)) {
         $ext = strtolower(pathinfo($file, PATHINFO_EXTENSION));
         if (in_array($ext, ['mp3', 'wav', 'ogg'])) {
             $name = pathinfo($file, PATHINFO_FILENAME);
+            
+            // FIX: Forza la codifica in UTF-8 per i caratteri accentati (es. "Espòòò")
+            $name_utf8 = mb_convert_encoding($name, 'UTF-8', 'UTF-8, ISO-8859-1, Windows-1252');
+            
             $songs[] = [
-                'name' => str_replace(['_', '-'], ' ', $name),
-                'file'  => $dir . $file,
-                'source' => 'Local Folder'
+                'name' => str_replace(['_', '-'], ' ', $name_utf8),
+                // FIX: Codifica il nome del file nell'URL per far funzionare spazi e accenti
+                'file'  => $dir . rawurlencode($file)
             ];
         }
     }
 }
 
-echo json_encode($songs);
+// FIX: Restituisce un array vuoto di fallback se json_encode fallisce invece di rompersi
+$json = json_encode($songs);
+echo $json ? $json : '[]';
 ?>
